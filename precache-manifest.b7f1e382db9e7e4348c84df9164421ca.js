4144053060172a3db1e9839491dd27683f2d42ed
adham2023 <adham.muhammadjonov1923@gmail.com>
2022-01-01 22:25:47 +0500

    deploy

---
+++ precache-manifest.b7f1e382db9e7e4348c84df9164421ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5586da605fedd33ad961",
+    "url": "/drawings/css/app.497c2ec3.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/drawings/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1a87252a7ccc14fa08b8559d783fb35f",
+    "url": "/drawings/index.html"
+  },
+  {
+    "revision": "54ff24d2d8649c3c07bb",
+    "url": "/drawings/js/about.f8006b1d.js"
+  },
+  {
+    "revision": "5586da605fedd33ad961",
+    "url": "/drawings/js/app.719c6e26.js"
+  },
+  {
+    "revision": "a43258a41395288b7c23",
+    "url": "/drawings/js/chunk-vendors.d72583f7.js"
+  },
+  {
+    "revision": "ad733c12843f0ae1acc4dc8f6177df8b",
+    "url": "/drawings/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/drawings/robots.txt"
+  }
+]);
