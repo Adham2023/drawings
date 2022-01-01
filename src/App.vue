17759f2e000ccd022f730b64b70c7188e898b061
adham2023 <adham.muhammadjonov1923@gmail.com>
2022-01-01 22:09:51 +0500

    qwerty

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="buttons">
+    <button style="margin-right: 1em" @click="undo">undo</button>
+    <button style="margin-right: 1em" @click="redo">redo</button>
+    <button style="margin-right: 1em" @click="clearcan">clear all</button>
     <button style="margin-right: 1em" @click="eraseData">Eraser aa</button>
     <input
       type="color"
@@ -11,40 +14,50 @@
   </div>
   <div class="canvaswrp">
     <canvas ref="can"></canvas>
-    <div v-if="appearOverlay" @dblclick="appearOverlay = !appearOverlay" class="overlay"></div>
+    <div
+      v-if="appearOverlay"
+      @dblclick="appearOverlay = !appearOverlay"
+      class="overlay"
+    ></div>
     <div class="ruler">
-      <div 
-        v-for="i in 300" :key="i" 
+      <div
+        v-for="i in 300"
+        :key="i"
         class="lines"
-        
-        :style="{width: (i % 10 === 0 ? 25: 17) + 'px'}"
-      >{{ i }}</div>
+        :style="{ width: (i % 10 === 0 ? 25 : 17) + 'px' }"
+      >
+        {{ i }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import { fabric } from "fabric";
-import {
-  addObj,
-  emitDrawing,
-  drawingOn,
-  emitMouseDown,
-  emitMouseUp,
-  newOnMouseDown,
-  newOnMouseUp,
-  emitColorChange,
-  newColorSet,
-  emitBrushWidthChange,
-  newBrushWidthSet,
-} from "./socket-client";
+// import {
+//   addObj,
+//   emitDrawing,
+//   drawingOn,
+//   emitMouseDown,
+//   emitMouseUp,
+//   newOnMouseDown,
+//   newOnMouseUp,
+//   emitColorChange,
+//   newColorSet,
+//   emitBrushWidthChange,
+//   newBrushWidthSet,
+// } from "./socket-client";
+import socketMixin from "./spcket-client.mixin";
 export default {
+  mixins: [socketMixin],
   data() {
     return {
       pathColor: "#3fff06",
       isErasing: false,
       prevColor: this.pathColor,
       appearOverlay: false,
+      state: [],
+      mods: 0,
     };
   },
   mounted() {
@@ -55,13 +68,64 @@ export default {
     }, 2000);
   },
   methods: {
+    undoLocal() {
+      console.log("undoLocal")
+      console.log(this.mods, this.state.length);
+      if (this.mods < this.state.length - 1) {
+        canvas.clear().renderAll();
+        canvas.loadFromJSON(this.state[this.state.length - 1 - this.mods - 1]);
+        canvas.renderAll();
+        this.mods += 1;
+      }
+    },
+    undo() {
+      console.log(this.mods, this.state.length);
+      if (this.mods < this.state.length - 1) {
+        canvas.clear().renderAll();
+        canvas.loadFromJSON(this.state[this.state.length - 1 - this.mods - 1]);
+        canvas.renderAll();
+        this.mods += 1;
+        this.emitUndo()
+      }
+    },
+    redoLocal() {
+      if (this.mods > 0) {
+        canvas.clear().renderAll();
+        canvas.loadFromJSON(this.state[this.state.length - 1 - this.mods + 1]);
+        canvas.renderAll();
+        this.mods -= 1;
+      }
+    },
+    redo() {
+      if (this.mods > 0) {
+        canvas.clear().renderAll();
+        canvas.loadFromJSON(this.state[this.state.length - 1 - this.mods + 1]);
+        canvas.renderAll();
+        this.mods -= 1;
+        this.emitRedo()
+      }
+    },
+    clearcanLocal() {
+      canvas.clear().renderAll();
+    },
+    clearcan() {
+      canvas.clear().renderAll();
+      this.emitClearAll()
+    },
+    updateModifications(savehistory) {
+      if (savehistory === true) {
+        let myjson = JSON.stringify(canvas);
+        this.state.push(myjson);
+        console.log(this.state.length)
+      }
+    },
     eraseData() {
       if (this.isErasing) {
         canvas.freeDrawingBrush.width = 5;
         this.pathColor = this.prevColor;
         canvas.freeDrawingBrush.color = this.pathColor;
-        emitColorChange(this.pathColor);
-        emitBrushWidthChange(5);
+        this.emitColorChange(this.pathColor);
+        this.emitBrushWidthChange(5);
         this.isErasing = false;
       } else {
         canvas.freeDrawingBrush.width = 15;
@@ -69,14 +133,14 @@ export default {
         this.prevColor = this.pathColor;
         this.pathColor = "black";
         canvas.freeDrawingBrush.color = this.pathColor;
-        emitColorChange(this.pathColor);
-        emitBrushWidthChange(15);
+        this.emitColorChange(this.pathColor);
+        this.emitBrushWidthChange(15);
       }
     },
     setColor(e) {
       this.pathColor = e.target.value;
       canvas.freeDrawingBrush.color = this.pathColor;
-      emitColorChange(this.pathColor);
+      this.emitColorChange(this.pathColor);
     },
     initFabric() {
       const ref = this.$refs.can;
@@ -86,12 +150,12 @@ export default {
         width: window.innerWidth,
         backgroundColor: "black",
       });
-      // canvas.setBackgroundColor({source: 'http://fabricjs.com/assets/escheresque_ste.png'}, 
-      // canvas.renderAll.bind(canvas));
+      canvas.counter = 0;
+      this.updateModifications(true);
     },
     setUpListeners() {
       let isDrwaing = false;
-      canvas.freeDrawingBrush.width = 5;
+      canvas.freeDrawingBrush.width = 3;
       canvas.freeDrawingBrush.color = this.pathColor;
       canvas
         .on("mouse:down", (e) => {
@@ -101,6 +165,9 @@ export default {
         .on("mouse:up", (e) => {
           isDrwaing = false;
           onMouseUp(e);
+          canvas.includeDefaultValues = false;
+          this.updateModifications(true);
+          canvas.counter++;
         })
         .on("mouse:move", (e) => {
           if (isDrwaing) {
@@ -108,26 +175,25 @@ export default {
             drawRealTime(e, pointer);
           }
         });
-
-      function onMouseDown(e) {
+      let onMouseDown = (e) => {
         const pointer = canvas.getPointer(e);
-        emitMouseDown({ pointer, e });
+        this.emitMouseDown({ pointer, e });
       }
-
-      function onMouseUp(e) {
+      let onMouseUp = (e) => {
         const pointer = canvas.getPointer(e);
-        emitMouseUp({ e, pointer });
+        this.emitMouseUp({ e, pointer });
       }
-
-      function drawRealTime(e, pointer) {
-        emitDrawing({ pointer, e });
+      let drawRealTime = (e, pointer) => {
+        this.emitDrawing({ pointer, e });
       }
-      newBrushWidthSet(canvas);
-      newOnMouseUp(canvas);
-      newOnMouseDown(canvas);
-      drawingOn(canvas);
-      addObj(canvas);
-      newColorSet(canvas, (newColor) => {
+      this.newBrushWidthSet(canvas);
+      this.newOnMouseUp(canvas);
+      this.newOnMouseDown(canvas);
+      this.drawingOn(canvas);
+      this.onClearAll()
+      this.newUndo()
+      this.newRedo()
+      this.newColorSet(canvas, (newColor) => {
         this.pathColor = newColor;
       });
     },
@@ -152,7 +218,7 @@ export default {
   display: flex;
   align-items: center;
   justify-content: flex-end;
-  padding: 0 1em
+  padding: 0 1em;
 }
 
 .canvaswrp {
---
+++ src/spcket-client.mixin.js
@@ -0,0 +1,84 @@
+import io from 'socket.io-client'
+// import { fabric } from 'fabric'
+
+// const socket = io("https://drawing-io.de.r.appspot.com")
+const socket = io("http://192.168.32.58:8180")
+
+export default {
+    methods: {
+        emitAdd(obj) {
+            socket.emit('object-added', obj)
+        },
+        emitModify(obj) {
+            socket.emit('object-modified', obj)
+        },
+        emitDrawing(obj) {
+            socket.emit('drawing-path', obj)
+        },
+        emitMouseUp(obj) {
+            socket.emit('on-mouse-up', obj)
+        },
+        emitMouseDown(obj) {
+            socket.emit('on-mouse-down', obj)
+        },
+        emitColorChange(obj) {
+            socket.emit('emit-color-change', obj)
+        },
+        emitBrushWidthChange(obj) {
+            socket.emit('emit-brush-width', obj)
+        },
+        emitUndo() {
+            socket.emit("undo-emitted")
+        },
+        emitRedo() {
+            socket.emit("redo-emitted")
+        },
+        emitClearAll() {
+            socket.emit("clear-all-emitted")
+        },
+        // listeners
+        onClearAll() {
+            socket.on("clear-all-set", () => {
+                this.clearcanLocal()
+            })
+        },
+        newUndo() {
+            socket.on('undo-set', () => {
+                console.log("new undo")
+                this.undoLocal()
+            })
+        },
+        newRedo() {
+            socket.on('redo-set', () => {
+                this.redoLocal()
+            })
+        },
+        newBrushWidthSet(canvas) {
+            socket.on('new-brush-width-set', newBurshWidth => {
+                canvas.freeDrawingBrush.width = newBurshWidth
+            })
+        },
+        newColorSet(canvas, cb) {
+            socket.on('new-color-set', newColor => {
+                canvas.freeDrawingBrush.color = newColor
+                cb(newColor)
+            })
+        },
+        newOnMouseDown(canvas) {
+            socket.on('new-on-mouse-down', ({ e, pointer }) => {
+                canvas.freeDrawingBrush.onMouseDown(pointer, e);
+            })
+        },
+        newOnMouseUp(canvas) {
+            socket.on('new-on-mouse-up', ({ e, pointer }) => {
+                canvas.freeDrawingBrush.onMouseUp(e, pointer);
+                this.updateModifications(true);
+            })
+        },
+        drawingOn(canvas) {
+            socket.on('new-drawingpath', data => {
+                canvas.freeDrawingBrush.onMouseMove(data.pointer, data.e);
+            })
+        }
+    }
+}
